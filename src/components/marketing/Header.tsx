7098225c486878efea63b5a8cb748ac12e4bb4f7
Eduardo Sotero <eduardo.sotero@toptal.com>
2023-05-08 21:56:24 -0300

    save changes

--- src/components/marketing/Header.tsx
+++ src/components/marketing/Header.tsx
@@ -80,7 +80,7 @@ function Header() {
       </nav>
       <Dialog as="div" className="lg:hidden" open={opened} onClose={close}>
         <div className="fixed inset-0 z-50" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
